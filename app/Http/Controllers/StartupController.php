9062cba298bc1a03c4e18b43bb39f566c3f9d5fe
Sivadas Rajan <sivadasrajan@gmail.com>
2022-11-05 19:17:08 +0530

    base structure

---
+++ app/Http/Controllers/StartupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Startup;
+use Illuminate\Http\Request;
+
+class StartupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Startup  $startup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Startup $startup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Startup  $startup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Startup $startup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Startup  $startup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Startup $startup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Startup  $startup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Startup $startup)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_11_05_132456_create_donations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('startup_id')->references('id')->on('startups');
+            $table->decimal('amount',20,4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+};
